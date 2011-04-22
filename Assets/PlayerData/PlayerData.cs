ec798601b73577c5b2f0d9b68162ae4097ffa7ab
junglist88 <junglist88@d0199e64-aae7-baa2-b816-e71a5e7f533c>
2011-04-22 02:55:11 +0000

    Added basic Heads Up Display with info on Minerals, ManPower, Unit Cost, and Time.  Added GUI Menus for creating buildings.  Can now properly check if a tower can be created (all pre conditions are passed) and then the tower is placed on the map.  Still have to get tower placement code in.

---
+++ Assets/PlayerData/PlayerData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerData
+{
+    public static int minerals = 100;
+    public static int manPower = 100;
+    public static int unitCount = 0;
+}
--- Assets/Tests/CreateBuildingTest1.cs
+++ Assets/Tests/CreateBuildingTest1.cs
@@ -1,99 +1,99 @@
-using UnityEngine;
-using System.Collections;
+//using UnityEngine;
+//using System.Collections;
 
-public class CreateBuildingTest1 : MonoBehaviour
-{
-    public Transform buildingPrefab; //the building that will be built
-    public Transform buildingPrefab_high; //the building in which we dont meet the specifications
-    private Transform building; //the instance of the new building
+//public class CreateBuildingTest1 : MonoBehaviour
+//{
+//    public Transform buildingPrefab; //the building that will be built
+//    public Transform buildingPrefab_high; //the building in which we dont meet the specifications
+//    private Transform building; //the instance of the new building
 
-    //obstacles used to test pre-condition of building creation placement
-    public Transform unitObstacle; 
-    public Transform buildingObstacle;
+//    //obstacles used to test pre-condition of building creation placement
+//    public Transform unitObstacle; 
+//    public Transform buildingObstacle;
 
-    bool run;
+//    bool run;
 
-    // Use this for initialization
-    void Start()
-    {
-        run = true;
-    }
+//    // Use this for initialization
+//    void Start()
+//    {
+//        run = true;
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (run)
-        {
-            //first test, place building in a clear location
-            building = Instantiate(buildingPrefab, transform.position, transform.rotation) as Transform;
-            if (IsBuildValid(building.position)) 
-                print("Building was placed successfully");
-            Destroy(building.gameObject);
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (run)
+//        {
+//            //first test, place building in a clear location
+//            building = Instantiate(buildingPrefab, transform.position, transform.rotation) as Transform;
+//            if (IsBuildValid(building.position)) 
+//                print("Building was placed successfully");
+//            Destroy(building.gameObject);
 
-            //second test, place building over an existing unit
-            building = Instantiate(buildingPrefab, unitObstacle.position, unitObstacle.rotation) as Transform;
-            if (IsBuildValid(building.position))
-                print("Building was placed successfully");
-            Destroy(building.gameObject);
+//            //second test, place building over an existing unit
+//            building = Instantiate(buildingPrefab, unitObstacle.position, unitObstacle.rotation) as Transform;
+//            if (IsBuildValid(building.position))
+//                print("Building was placed successfully");
+//            Destroy(building.gameObject);
 
-            //third test, place building over an existing building
-            building = Instantiate(buildingPrefab, buildingObstacle.position, buildingObstacle.rotation) as Transform;
-            if (IsBuildValid(building.position))
-                print("Building was placed successfully");
-            Destroy(building.gameObject);
+//            //third test, place building over an existing building
+//            building = Instantiate(buildingPrefab, buildingObstacle.position, buildingObstacle.rotation) as Transform;
+//            if (IsBuildValid(building.position))
+//                print("Building was placed successfully");
+//            Destroy(building.gameObject);
 
-            //fourth test, attempt to build a building without proper requirements
-            building = Instantiate(buildingPrefab_high, buildingObstacle.position, buildingObstacle.rotation) as Transform;
-            if (IsBuildValid(building.position))
-                print("Building was placed successfully");
-            Destroy(building.gameObject);
+//            //fourth test, attempt to build a building without proper requirements
+//            building = Instantiate(buildingPrefab_high, buildingObstacle.position, buildingObstacle.rotation) as Transform;
+//            if (IsBuildValid(building.position))
+//                print("Building was placed successfully");
+//            Destroy(building.gameObject);
 
-            //fifth test, attempt to build a building with too low of resources
-            PlayerData.minerals = 0;
-            PlayerData.manPower = 0;
-            building = Instantiate(buildingPrefab, transform.position, transform.rotation) as Transform;
-            if (IsBuildValid(building.position))
-                print("Building was placed successfully");
-            Destroy(building.gameObject);
+//            //fifth test, attempt to build a building with too low of resources
+//            PlayerData.minerals = 0;
+//            PlayerData.manPower = 0;
+//            building = Instantiate(buildingPrefab, transform.position, transform.rotation) as Transform;
+//            if (IsBuildValid(building.position))
+//                print("Building was placed successfully");
+//            Destroy(building.gameObject);
 
-            run = false;
-        }
-    }
+//            run = false;
+//        }
+//    }
 
-    private bool IsBuildValid(Vector3 _position)
-    {
-        //check if a unit or building is in the way
-        Collider[] colliders = Physics.OverlapSphere(_position, 10); //get all objects in a 10 unit radius
-        foreach (Collider target in colliders)
-        {
-            if (target.tag == "Building" || target.tag == "Unit") //in range of unit, cannot build
-            {
-                print("Cannot place Building, a unit or building is in the way");
-                return false;
-            }
-        }
+//    private bool IsBuildValid(Vector3 _position)
+//    {
+//        //check if a unit or building is in the way
+//        Collider[] colliders = Physics.OverlapSphere(_position, 10); //get all objects in a 10 unit radius
+//        foreach (Collider target in colliders)
+//        {
+//            if (target.tag == "Building" || target.tag == "Unit") //in range of unit, cannot build
+//            {
+//                print("Cannot place Building, a unit or building is in the way");
+//                return false;
+//            }
+//        }
 
-        //check if you have sufficient funds
-        if (PlayerData.minerals - building.GetComponent<Building>().MineralCost < 0) //dont have enough rescources
-        {
-            print("Cannot place Building, insufficient resources");
-            return false;
-        }
+//        //check if you have sufficient funds
+//        if (PlayerData.minerals - building.GetComponent<Building>().MineralCost < 0) //dont have enough rescources
+//        {
+//            print("Cannot place Building, insufficient resources");
+//            return false;
+//        }
 
-        //check if you have sufficient funds
-        if (PlayerData.manPower - building.GetComponent<Building>().ManPowerCost < 0) //dont have enough rescources
-        {
-            print("Cannot place Building, insufficient resources");
-            return false;
-        }
+//        //check if you have sufficient funds
+//        if (PlayerData.manPower - building.GetComponent<Building>().ManPowerCost < 0) //dont have enough rescources
+//        {
+//            print("Cannot place Building, insufficient resources");
+//            return false;
+//        }
 
-        //check if requirements are met to build this tower
-        if(!building.GetComponent<Building>().RequirementsMet())
-        {
-            print("Cannot place Building, requirements for this Building have not been met");
-            return false;
-        }
+//        //check if requirements are met to build this tower
+//        if(!building.GetComponent<Building>().RequirementsMet())
+//        {
+//            print("Cannot place Building, requirements for this Building have not been met");
+//            return false;
+//        }
 
-        return true;
-    }
-}
+//        return true;
+//    }
+//}
