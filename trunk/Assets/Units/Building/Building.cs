17cd3a7625502992f1da998fe27dd017673623b8
junglist88 <junglist88@d0199e64-aae7-baa2-b816-e71a5e7f533c>
2011-04-22 10:07:33 +0000

    Added menu layouts for Buildings, now have foundation completed for menus for creating buildings and units. Just need to actually place the units now.

--- trunk/Assets/Units/Building/Building.cs
+++ trunk/Assets/Units/Building/Building.cs
@@ -3,18 +3,46 @@ using System.Collections;
 
 public class Building : MonoBehaviour
 {
+    private enum state { DEFAULT, CREATE_UNIT }
+    private state State;
+
     //building specifications
     public int MineralCost;
     public int ManPowerCost;
     public float MaxHealth;
-
     private float currentHealth;
-    
+
+    //ghost effect
+    private ArrayList renderList = new ArrayList(); //hold the render materials that will be used to create the transparent look when building
+    private Renderer[] renderArray; //array that will be created by renderList, holds the render objects
+    private Color[] originalColor;
+
+    //create building variables
+    private bool validLocation;
+    public bool ValidLocation
+    {
+        get { return validLocation; }
+    }
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
+        //set states
+        State = state.DEFAULT;
 
+        //initial creation of building
+        validLocation = false;
+
+        //find / add render objects, used for creating transparent effect
+        if (renderer != null)
+            renderList.Add(renderer);
+        GetRenderObjects(transform); //set the renderArray, used to enable / disable the transparent look
+        renderArray = (Renderer[])renderList.ToArray(typeof(Renderer));
+
+        //save the originalColor this is used with the ghosting effect
+        originalColor = new Color[renderArray.Length];
+        for(int i=0; i<renderArray.Length; i++)
+            originalColor[i] = renderArray[i].material.color;
     }
 
     // Update is called once per frame
@@ -30,5 +58,98 @@ public class Building : MonoBehaviour
     public bool RequirementsMet()
     {
         return true;
+    }
+
+    /// <summary>
+    /// Recursively sets the renderArray with all Render Objects in the current object, this includes all children
+    /// </summary>
+    /// <param name="_transform">Current Transform object</param>
+    private void GetRenderObjects(Transform _transform)
+    {
+        if (_transform == null)
+            return;
+
+        foreach (Transform child in _transform) //check all children for render objects
+        {
+            if (child.renderer != null) //a renderer exists so save this object
+                renderList.Add(child.renderer);
+            GetRenderObjects(child); //recursively check if childrens children have render objects
+        }
+    }
+
+    /// <summary>
+    /// Turns on the transparent look on the the render materials
+    /// </summary>
+    public void EnableTransparentEffect()
+    {
+        for (int i = 0; i < renderArray.Length; i++)
+        {
+            //turn on transparent effect
+            foreach (Material material in ((Renderer)renderArray[i]).materials)
+            {
+                material.shader = Shader.Find("Transparent/Diffuse");
+                material.color = new Color(material.color.r, material.color.g, material.color.b, .5F);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Turns off the transparent look on the render materials
+    /// </summary>
+    public void DisableTransparentEffect()
+    {
+        for (int i = 0; i < renderArray.Length; i++)
+        {
+            //turn off transparent effect
+            foreach (Material material in ((Renderer)renderArray[i]).materials)
+            {
+                material.shader = Shader.Find("Diffuse");
+                material.color = new Color(material.color.r, material.color.g, material.color.b, 1F);
+            }
+        }
+        RestoreColor();
+    }
+    
+    /// <summary>
+    /// Changes the color of all materials in this building
+    /// </summary>
+    /// <param name="_color">Color you wish to set the materials to</param>
+    private void ChangeColor(Color _color)
+    {
+        for (int i = 0; i < renderArray.Length; i++)
+        {
+            foreach (Material material in ((Renderer)renderArray[i]).materials)
+                material.color = new Color(_color.r, _color.g, _color.b, material.color.a);
+        }
+    }
+
+    /// <summary>
+    /// Restores the color of all materials to its original colors
+    /// </summary>
+    private void RestoreColor()
+    {
+        for (int i = 0; i < renderArray.Length; i++)
+        {
+            foreach (Material material in ((Renderer)renderArray[i]).materials)
+                material.color = originalColor[i];
+        }
+    }
+
+    /// <summary>
+    /// Places the building in a valid build state
+    /// </summary>
+    public void SetValidLocation()
+    {
+        validLocation = true;
+        ChangeColor(Color.green);
+    }
+
+    /// <summary>
+    /// Places the building in a invalid build state
+    /// </summary>
+    public void SetInValidLocation()
+    {
+        validLocation = false;
+        ChangeColor(Color.red);
     }
 }
--- trunk/Assets/Units/Building/BuildingParent.cs
+++ trunk/Assets/Units/Building/BuildingParent.cs
@@ -3,15 +3,35 @@ using System.Collections;
 
 public class BuildingParent : MonoBehaviour
 {
+    private enum stateGUI { DEFAULT, OPEN }
+    private stateGUI StateGUI;
+
+    //build Menu variables
+    private int buildingNum;
+    public int menuButtonWidth;
+    public int menuButtonHeight;
+    public int menuButtonBufferY;
+    public int menuButtonBufferX;
+
+    [System.Serializable]
+    public class BuildingMenu { public string[] ButtonStrings; };
+    public BuildingMenu[] BuildingStrings; // [Building Name, Array of Strings for each building]
+
+    private int menuWidth;
+    private int menuHeight;
+    private int[,] menuButtonPos; //[icon number, {x,y}]
+    private int[] menuPos = new int[2]; //[0] = x coordinate, [1]= y coordinate
 
     public Transform[] Buildings; //list of all the buildings that can be built 
+    public Transform userControl; //the UserControl object
 
-    
+
 
     // Use this for initialization
     void Start()
     {
-
+        //set states
+        StateGUI = stateGUI.DEFAULT;
     }
 
     // Update is called once per frame
@@ -33,11 +53,11 @@ public class BuildingParent : MonoBehaviour
             if (name == Buildings[i].name && BuildingPreCondition(Buildings[i])) //found the building and the building can be built
             {
                 //TO DO: set that this building has been built for requirements met purposes
-                
+
                 //lower resources
                 PlayerData.manPower -= Buildings[i].GetComponent<Building>().ManPowerCost;
                 PlayerData.minerals -= Buildings[i].GetComponent<Building>().MineralCost;
-                return Buildings[i];                
+                return Buildings[i];
             }
         }
 
@@ -65,12 +85,90 @@ public class BuildingParent : MonoBehaviour
         }
 
         //check requirements
-        if(!_building.GetComponent<Building>().RequirementsMet()) //failed
+        if (!_building.GetComponent<Building>().RequirementsMet()) //failed
         {
             print("CANNOT CREATE BUILDING: REQUIREMENTS NOT MET");
             return false;
         }
 
         return true;
+    }
+
+    /// <summary>
+    /// Name of the building of whoms menu to open
+    /// </summary>
+    /// <param name="name">Name of the building</param>
+    public bool OpenBuildingMenu(string name)
+    {
+        //find the requested building
+        for (int i = 0; i < Buildings.Length; i++)
+        {
+            if (name.Contains(Buildings[i].name)) //found the building
+            {
+                buildingNum = i;
+                StateGUI = stateGUI.OPEN;
+
+                //build menu initialization
+                menuWidth = 20 + 20 + menuButtonWidth;
+                menuHeight = 40 + (BuildingStrings[buildingNum].ButtonStrings.Length * (menuButtonHeight + menuButtonBufferY));
+                menuPos[0] = Screen.width - menuWidth - 10;
+                menuPos[1] = Screen.height - menuHeight;
+                menuButtonPos = new int[BuildingStrings[buildingNum].ButtonStrings.Length, 2];
+                for (int j = 0; j < BuildingStrings[buildingNum].ButtonStrings.Length; j++)
+                {
+                    menuButtonPos[j, 0] = menuPos[0] + menuButtonBufferX;
+                    menuButtonPos[j, 1] = menuPos[1] + (j * menuButtonBufferY) + (j * menuButtonHeight) + menuButtonBufferX * 2;
+                }
+
+                return true;
+            }
+        }
+
+        print("INVALID BUILDING NAME");
+        return false;
+    }
+
+    /// <summary>
+    /// Draws the GUI items, this is called every fram
+    /// </summary>
+    void OnGUI()
+    {
+        switch (StateGUI)
+        {
+            case stateGUI.OPEN:
+                BuildingMenuGUI();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void BuildingMenuGUI()
+    {
+        GUI.Box(new Rect(menuPos[0], menuPos[1], menuWidth, menuHeight), Buildings[buildingNum].name);
+
+        //draw all the build buttons
+        for (int i = 0; i < BuildingStrings[buildingNum].ButtonStrings.Length; i++)
+        {
+            if (GUI.Button(new Rect(menuButtonPos[i, 0], menuButtonPos[i, 1], menuButtonWidth, menuButtonHeight), BuildingStrings[buildingNum].ButtonStrings[i]))
+            {
+                if (i == BuildingStrings[buildingNum].ButtonStrings.Length - 1) //cancel button pressed
+                {
+                    StateGUI = stateGUI.DEFAULT;
+                    userControl.GetComponent<UserControl>().EnterDefaultState();
+                    return;
+                }
+
+                //create unit
+                print("pressed " + BuildingStrings[buildingNum].ButtonStrings[i]);
+                return;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Escape)) //escape button
+            {
+                StateGUI = stateGUI.DEFAULT;
+                userControl.GetComponent<UserControl>().EnterDefaultState();
+            }
+        }
     }
 }
