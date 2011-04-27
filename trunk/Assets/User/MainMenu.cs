8b0c6f82b7a3772b7f10a8f1acae05d8a2c85499
will.schaefer <will.schaefer@d0199e64-aae7-baa2-b816-e71a5e7f533c>
2011-04-27 13:39:17 +0000

    Main menu graphic / scene (no buttons or gui or anything useful yet)

---
+++ trunk/Assets/User/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+class MainMenu : MonoBehaviour
+    {
+
+        void OnGUI()
+        {
+            // Make a background box
+            GUI.Box(new Rect(10, 10, 100, 90), "");
+
+            // Make a button if its pressed load the MainLevel
+            if (GUI.Button(new Rect(20, 40, 80, 20), "Start Game"))
+            {
+                Application.LoadLevel("MainLevel");
+            }
+        }
+    }
+
